c124839e178072735135ede34211f8e0d463f760
Kevin Walker <k.walker@sae-sf.net>
2022-12-06 22:42:34 +0100

    Add submodule glew

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,13 @@ int main()
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.0f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
